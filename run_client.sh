a3175818baa041f8f40c400f9b9a3904ac1083f4
Joshua Spence <josh@joshuaspence.com>
2012-03-22 08:38:38 +1100

    Improved run_client and run_server scripts to check if ant is installed, and use hardcoded classpath and JAR directories if ant is not installed.

--- run_client.sh
+++ run_client.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
-# Configuration
+# Java configuration
 JRE=java
 JRE_FLAGS="-enableassertions -enablesystemassertions"
 
+# ANT configuration
+HAS_ANT=0
+ANT=$(which ant)
+[ "$?" -eq 0 ] && HAS_ANT=1
+
 # TODO: Get classpath
-CLASSPATH=
+if [ HAS_ANT -eq 1 ]; then
+	CLASSPATH=
+else
+	CLASSPATH=""
+fi
 
 # TODO: JAR file (specified relative to script)
-CLIENT_JAR=
+if [ HAS_ANT -eq 1 ]; then
+	CLIENT_JAR=
+else
+	CLIENT_JAR="`dirname $0`/build/classes/ELEC5616_client.jar"
+fi
 
 $JRE -classpath $CLASSPATH $JRE_FLAGS -jar $CLIENT_JAR
--- run_server.sh
+++ run_server.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
-# Configuration
+# Java configuration
 JRE=java
 JRE_FLAGS="-enableassertions -enablesystemassertions"
 
+# ANT configuration
+HAS_ANT=0
+ANT=$(which ant)
+[ "$?" -eq 0 ] && HAS_ANT=1
+
 # TODO: Get classpath
-CLASSPATH=
+if [ HAS_ANT -eq 1 ]; then
+	CLASSPATH=
+else
+	CLASSPATH=""
+fi
 
 # TODO: JAR file (specified relative to script)
-SERVER_JAR=
+if [ HAS_ANT -eq 1 ]; then
+	SERVER_JAR=
+else
+	SERVER_JAR="`dirname $0`/build/classes/ELEC5616_server.jar"
+fi
 
 $JRE -classpath $CLASSPATH $JRE_FLAGS -jar $SERVER_JAR
