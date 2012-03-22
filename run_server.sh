a9da6aa856af7f0d982432cf3989f38ac2aee5e4
Joshua Spence <josh@joshuaspence.com>
2012-03-22 12:21:58 +1100

    Fixed up run_client and run_server scripts.

--- run_server.sh
+++ run_server.sh
@@ -5,22 +5,30 @@ JRE=java
 JRE_FLAGS="-enableassertions -enablesystemassertions"
 
 # ANT configuration
-HAS_ANT=0
 ANT="$(which ant) -quiet"
-[ "$?" -eq 0 ] && HAS_ANT=1
+[ "$?" -eq 0 ] && HAS_ANT=1 || HAS_ANT=0
 
-# TODO: Get classpath
-if [ HAS_ANT -eq 1 ]; then
-	CLASSPATH=
+# Get classpath
+if [ $HAS_ANT -eq 1 ]; then
+	# Get classpath from ant
+	CLASSPATH=`$ANT classpath | sed -n "s/.*Classpath: '\(.*\)'/\1/p"`
 else
+	# Use default classpath
 	CLASSPATH=""
 fi
 
-# TODO: JAR file (specified relative to script)
-if [ HAS_ANT -eq 1 ]; then
-	SERVER_JAR=
+# Add -classpath prefix to classpath
+[ -n "$CLASSPATH" ] && CLASSPATH="-classpath $CLASSPATH" || CLASSPATH=""
+
+# Get JAR file (specified relative to script)
+JAR_DIR=`dirname $0`
+if [ $HAS_ANT -eq 1 ]; then
+	# Get JAR path from ant
+	JAR_DIR="$CLIENT_DIR`$ANT jar_dir | sed -n "s/.*JAR directory: '\(.*\)'/\1/p"`"
 else
-	SERVER_JAR="`dirname $0`/build/classes/ELEC5616_server.jar"
+	# Use default JAR path
+	JAR_DIR="$CLIENT_DIR/build/classes"
 fi
 
-$JRE -classpath $CLASSPATH $JRE_FLAGS -jar $SERVER_JAR
+# Execute
+$JRE -classpath $CLASSPATH $JRE_FLAGS -jar $JAR_DIR/ELEC5616_server.jar &
